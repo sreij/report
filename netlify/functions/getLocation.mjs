4f831c9c83519ffd816f6240dccc323ad95f9ea5
sreij <chrj23020@g.nihon-u.ac.jp>
2024-12-29 16:36:23 +0900

    query

--- netlify/functions/getLocation.mjs
+++ netlify/functions/getLocation.mjs
@@ -1,7 +1,7 @@
 export default async (req) => {
     const params = new URL(req.url).searchParams;
-    const latitude = +params.get('lat');
-    const longitude = +params.get('lon');
+    const latitude = parseFloat(params.get('lat'));
+    const longitude = parseFloat(params.get('lon'));
     const username = 'chrj23020';
     try {
         const response = await fetch(`http://api.geonames.org/findNearbyPlaceNameJSON?lat=${latitude}&lng=${longitude}&username=${username}`);
@@ -11,9 +11,9 @@ export default async (req) => {
         const data = await response.json();
         const placeName = data.geonames && data.geonames.length > 0 ? data.geonames[0].name : 'Location not found';
 
-        return new Response(json.stringify({placeName: {placeName}}));
+        return new Response(JSON.stringify({placeName: {placeName}}));
     } catch (error) {
-        return new Response( json.stringify({
+        return new Response( JSON.stringify({
             placeName: JSON.stringify({ error: error.message })
         }));
     }
